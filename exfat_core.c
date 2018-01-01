8aba7e36589f57d934d1ea2ae2c674a54a3279ec
Andreas Schneider <asn@cryptomilk.org>
2018-01-01 22:00:47 +0100

    Return a valid count in count_used_clusters()
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- exfat_core.c
+++ exfat_core.c
@@ -2107,6 +2107,9 @@ s32 exfat_count_used_clusters(struct super_block *sb)
 		}
 	}
 
+	if ((p_fs->num_clusters - 2) < (s32)count)
+		count = p_fs->num_clusters - 2;
+
 	return count;
 } /* end of exfat_count_used_clusters */
 
